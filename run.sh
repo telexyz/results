1d4209db034d67d05009f6b42ad784627c17cfbd
Tien Dung <dungtn@gmail.com>
2021-07-22 13:20:26 +0700

    play with kenlm

--- run.sh
+++ run.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
-# split -l6000000 ../data/news_titles.txt news_titles.
-./bin/telexify news_titles.aa news_titles.aa.xyz
+# Tách riêng 6 triệu titles đầu tiên
+# split -l6000000 ../data/news_titles.txt news_titles
+
+# Tách cụm âm tiết tiếng Việt theo dòng
+# ./bin/telexify news_titles.aa news_titles.aa.xyz
+
+# - - -
+
+# Thử N-gram với https://kheafield.com/code/kenlm
+# ./bin/lmplz -o 4 --text news_titles.aa.xyz --arpa news_titles.xyz.arpa # counting
+
+# Read Probing at https://kheafield.com/code/kenlm/structures
+# ./bin/build_binary -p 1.5 probing news_titles.xyz.arpa news_titles.xyz.binary 
+
+./bin/query -n -v summary -v sentence -v word news_titles.xyz.binary <query.txt
