4c198acaa15525fdbccc4b000c3833f04f33ee5f
Tien Dung <dungtn@gmail.com>
2021-07-23 09:50:40 +0700

    prepare data and n-gram exp

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-# Tách riêng 6 triệu titles đầu tiên
-# split -l6000000 ../data/news_titles.txt news_titles
+# Tách riêng 6 triệu titles đầu tiên từ file gốc
+# split -l6000000 ../data/news_titles.txt news_titles.
+# Hoặc giải nén file news_titles.aa.7z
 
 # Tách cụm âm tiết tiếng Việt theo dòng
 # ./bin/telexify news_titles.aa news_titles.aa.xyz
@@ -9,9 +10,14 @@
 # - - -
 
 # Thử N-gram với https://kheafield.com/code/kenlm
-# ./bin/lmplz -o 4 --text news_titles.aa.xyz --arpa news_titles.xyz.arpa # counting
+# ./bin/lmplz -o 6 --prune 3 --text news_titles.aa.xyz --arpa news_titles.xyz.arpa
 
 # Read Probing at https://kheafield.com/code/kenlm/structures
-# ./bin/build_binary -p 1.5 probing news_titles.xyz.arpa news_titles.xyz.binary 
+./bin/build_binary -p 1.5 probing news_titles.xyz.arpa news_titles.xyz.binary 
 
-./bin/query -n -v summary -v sentence -v word news_titles.xyz.binary <query.txt
+# Test với 1 câu từ tập news_titles.ab
+# "Nhiều hệ lụy khôn lường từ việc làm giả thực phẩm chức năng."
+./bin/query -v summary -v sentence -v word news_titles.xyz.binary <query.txt
+
+# pip3 install https://github.com/kpu/kenlm/archive/master.zip
+python3 query.py
